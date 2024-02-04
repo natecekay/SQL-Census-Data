928c8771c98aa982d2de528f841574953a19ef44
Nathaniel Cekay <100001314+natecekay@users.noreply.github.com>
2024-02-03 21:49:31 -0800

    census.sql

--- census.sql
+++ census.sql
@@ -1,3 +1,6 @@
+/* SQL Analysis of US Census data from 2000 and 2010 
+Author: Nathaniel Cekay*/
+
 CREATE TABLE census_data (
 	zipcode varchar(25),
 	geo_id varchar(100),
@@ -9,15 +12,13 @@ CREATE TABLE census_data (
 );
 
 /* To prepare, I checked the data in Excel and 
-arranged the columns in the same order. I also added a column for year,
+arranged the columns in the same order. I also added a column for the year,
 designating the 2000 census from the 2010 census */
 /* Importing the 2000 census data */
 
 COPY census_data
 FROM 'C:\Datasets\population_by_zip_2000.csv'
 WITH (FORMAT CSV, HEADER);
-
-
 
 /* Importing the 2010 census data */
 
@@ -26,7 +27,6 @@ SELECT * FROM census_data;
 COPY census_data
 FROM 'C:\Datasets\population_by_zip_2010.csv'
 WITH (FORMAT CSV, HEADER);
-
 
 /* Removing the empty rows from the data */
 
@@ -39,10 +39,7 @@ DROP COLUMN maximum_age;
 ALTER TABLE census_data
 DROP COLUMN gender;
 
-
 SELECT * FROM census_data;
-
-
 
 /* Checking for missing values */
 
@@ -58,14 +55,13 @@ SELECT *
 FROM census_data
 WHERE population IS NULL;
 
-/* After removing the empty columns, there were not additional missing values*/
+/* After removing the empty columns, there were no additional missing values */
 
 SELECT * FROM census_data;
 
+/* The data did not contain any Zip code values less than 5 digits */
 
-/* The data did not contain any zipcode values less than 5 digits*/
-
-/* Here I will get rid of extra characters in the zipcode column*/
+/* Here I will get rid of extra characters in the Zip code column */
 UPDATE census_data
 SET zipcode = REPLACE(zipcode, '860000US', '');
 
@@ -76,16 +72,16 @@ SET zipcode = REPLACE(zipcode, '8600000US', '');
 
 DELETE FROM census_data WHERE zipcode LIKE '%XX%'
 
-/* Next I will check the data for short zip code values, less than 5 digits*/
+/* Next I will check the data for short Zip code values, less than 5 digits*/
 
 SELECT *
 FROM census_data
 WHERE length(zipcode) <5;
 
-/* The data did not contain any zipcode values less than 5 digits*/
+/* The data did not contain any ZIP code values less than 5 digits*/
 
 
-/*here I return a list of zipcodes that appeared twice in the census */
+/*here I return a list of Zip codes that appeared twice in the census */
 
 SELECT zipcode, COUNT(zipcode)
 FROM census_data
@@ -99,14 +95,13 @@ ALTER TABLE census_data
 ALTER COLUMN zipcode TYPE numeric
 USING zipcode::numeric;
 
-/* Next I examine correlation between zipcode population */
+/* Next I examine the correlation between Zip code population */
 
 SELECT corr(zipcode, population)
     AS zipcode_and_population
 FROM census_data;
 
-/* there is a very weak correlation of 0.064 between zipcode and population*/
-
+/* there is a very weak correlation of 0.064 between Zip code and population*/
 
 /* Here I calculate the average population*/
 
@@ -115,18 +110,15 @@ FROM census_data;
 
 /* The average population is 9475 people*/
 
-
 /* Here I select the zipcode with the highest population*/
 
 SELECT *
 FROM census_data 
 WHERE population = ( SELECT MAX(population) FROM census_data );
 
-
 /* The result is 10456, the Bronx, NY, with 86547 people in 2010*/
 
-
-/* Here I look at the 5 zipcodes with the highest populations*/
+/* Here I look at the 5 Zip codes with the highest populations*/
 
 SELECT *
 FROM census_data 
@@ -136,7 +128,7 @@ LIMIT 5;
 /* The results are 10456 in 2010, 60620 in 2000, 60804 in 2010,
 60505 in 2010, and 60640 in 2000 */
 
-/* Here I look at the 5 zipcodes with the lowest populations*/
+/* Here I look at the 5 Zip codes with the lowest populations*/
 
 SELECT *
 FROM census_data 
@@ -145,8 +137,6 @@ LIMIT 5;
 
 /* the results are 24846 in 2000, 49263 in 2000, 97604 in 2000,
 27841 in 2000, and 42759 in 2000 */
-
-
 
 /* Here I use a subquery to view the top 90th percentile cutoff for total_sales*/
 
@@ -158,7 +148,5 @@ WHERE population >= (
 	)
 ORDER BY population DESC;
 
-/* This returns the 100 zipcodes with the largest population,
+/* This returns the 100 Zip codes with the largest population,
 in other words the top 90th percentile cutoff for population*/
-
-
